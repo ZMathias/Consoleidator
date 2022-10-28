c4f09ca2437cc964e86bcba01702f02a81661091
ZMathias <zagoni.matyas-elemer@bathory.ro>
2022-10-28 21:37:15 +0300

    Added startup functionality and accomodating dialogs.

--- runner/main.cpp
+++ runner/main.cpp
@@ -34,7 +34,11 @@ BOOL ToggleTray(HWND, HINSTANCE hInstance);
 BOOL IsConsoleWindow(HWND hWnd);
 BOOL InjectDllIntoWindow(unsigned int uiMode, HWND hForeground, const wchar_t* optArg = nullptr);
 BOOL ShowTitleSetter();
+BOOL AddToStartup();
 std::string ToAscii(const std::wstring& str);
+void SetStartupCheckboxGreyed(HWND, bool);
+void SetStartupCheckmark(HWND, bool);
+void ToggleStartup(HWND hWnd);
 
 int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ PWSTR pCmdLine, _In_ int nCmdShow)
 {
@@ -175,6 +179,142 @@ BOOL AddToStartup()
 		logger::LogError("Access denied writing to registry.", __FILE__, __LINE__);
         return FALSE;
 	}
+
+	return TRUE;
+}
+
+BOOL RemoveFromStartup()
+{
+	WCHAR wPathToImage[500]{};
+	GetModuleFileNameW(nullptr, wPathToImage, 500);
+
+	HKEY hKey{};
+	LRESULT lResult = 
+		RegOpenKeyExW(HKEY_CURRENT_USER, L"Software\\Microsoft\\Windows\\CurrentVersion\\Run", NULL, KEY_ALL_ACCESS, &hKey);
+
+	if (lResult != ERROR_SUCCESS) 
+	{
+	    if (lResult == ERROR_FILE_NOT_FOUND) {
+	        MessageBoxW(nullptr, L"Unable to delete from startup registry.\nCheck if the program has enough privileges.", L"Error", MB_ICONERROR);
+			logger::LogError("Registry key not found while opening startup registry key", __FILE__, __LINE__);
+	    	return TRUE;
+	    } 
+	    else {
+	        MessageBoxW(nullptr, L"Unable to delete from startup registry.\nCheck if the program has enough privileges.", L"Error", MB_ICONERROR);
+			logger::LogError("Access denied writing to registry.", __FILE__, __LINE__);
+	        return FALSE;
+	    }
+	}
+
+	lResult = RegDeleteValueW(hKey, L"Consoleidator");
+
+	if (lResult != ERROR_SUCCESS) 
+	{
+		if (lResult == ERROR_FILE_NOT_FOUND) {
+			MessageBoxW(nullptr, L"Unable to delete from startup registry.\nIt probably wasn't present.\nCheck the log for more information.", L"Error", MB_ICONERROR);
+			logger::LogError("Unable to delete value \"Consoleidator\" due to reason: ERROR_FILE_NOT_FOUND", __FILE__, __LINE__);
+			return FALSE;
+		} else {
+			MessageBoxW(nullptr, L"Unable to delete from startup registry.\nCheck if the program has enough privileges.", L"Error", MB_ICONERROR);
+			logger::LogError("Access denied writing to registry.", __FILE__, __LINE__);
+			return FALSE;
+		}
+        return FALSE;
+	}
+
+	return TRUE;
+}
+
+void ToggleStartup(HWND hWnd)
+{
+	HMENU hmenu = GetMenu(hWnd);
+
+	MENUITEMINFO menuItem{};
+	menuItem.cbSize = sizeof MENUITEMINFO;
+	menuItem.fMask = MIIM_STATE;
+
+	GetMenuItemInfo(hmenu, ID_OPTIONS_RUNATSTARTUP, FALSE, &menuItem);
+
+	if (menuItem.fState == MFS_CHECKED) {
+		// Checked, uncheck it
+		menuItem.fState = RemoveFromStartup() ? MFS_UNCHECKED : MFS_CHECKED;
+	} else {
+		// Unchecked, check it
+		menuItem.fState = AddToStartup() ? MFS_CHECKED : MFS_UNCHECKED;
+	}
+
+	SetMenuItemInfo(hmenu, ID_OPTIONS_RUNATSTARTUP, FALSE, &menuItem);
+}
+
+void SetStartupCheckboxGreyed(HWND hWnd, bool state)
+{
+	// toggle menu item
+	HMENU hMenu{};
+	hMenu = GetMenu(GetActiveWindow());
+
+	MENUITEMINFO menuInfo{};
+	menuInfo.cbSize = sizeof MENUITEMINFO;
+	menuInfo.fMask = MIIM_STATE;
+	GetMenuItemInfo(hMenu, 0, FALSE, &menuInfo);
+	menuInfo.fState = state ? MFS_ENABLED : MFS_DISABLED;
+	SetMenuItemInfo(hMenu, 0, FALSE, &menuInfo);
+}
+
+void SetStartupCheckmark(HWND hWnd, bool state)
+{
+	HMENU hmenu = GetMenu(hWnd);
+
+	MENUITEMINFO menuItem{};
+	menuItem.cbSize = sizeof MENUITEMINFO;
+	menuItem.fMask = MIIM_STATE;
+
+	GetMenuItemInfo(hmenu, ID_OPTIONS_RUNATSTARTUP, FALSE, &menuItem);
+
+	menuItem.fState = state ? MFS_CHECKED : MFS_UNCHECKED;
+
+	SetMenuItemInfo(hmenu, ID_OPTIONS_RUNATSTARTUP, FALSE, &menuItem);
+}
+
+BOOL IsStartup()
+{
+	HKEY hKey{};
+	LRESULT lResult = 
+		RegOpenKeyExW(HKEY_CURRENT_USER, L"Software\\Microsoft\\Windows\\CurrentVersion\\Run", NULL, KEY_ALL_ACCESS, &hKey);
+
+	if (lResult != ERROR_SUCCESS) 
+	{
+	    if (lResult == ERROR_FILE_NOT_FOUND) {
+			MessageBoxW(nullptr, L"Unable to check for startup presence in the registry. The startup button has been disabled.", L"Error", MB_ICONWARNING);
+			logger::LogError("Registry key not found while opening startup registry key", __FILE__, __LINE__);
+	    	return TRUE;
+	    } 
+	    else {
+	    	MessageBoxW(nullptr, L"Unable to check for startup presence in the registry. The startup button has been disabled.", L"Error", MB_ICONWARNING);
+			// disable the startup checkbox
+			logger::LogError("Access denied writing to registry.", __FILE__, __LINE__);
+	        return FALSE;
+	    }
+	}
+
+	lResult = RegQueryValueExW(
+		hKey,
+		L"Consoleidator",
+		nullptr,
+		nullptr,
+		nullptr,
+		nullptr
+	);
+
+	if (lResult != ERROR_SUCCESS)
+	{
+		if (lResult == ERROR_FILE_NOT_FOUND) {
+			return FALSE;
+		} else {
+			logger::LogError("An error occured while checking for startup presence in the registry.", __FILE__, __LINE__);
+			return FALSE;
+		}
+	}
+	 return TRUE;
 }
 
 std::string ToAscii(const std::wstring& str)
@@ -297,6 +437,8 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 				return 0;
 			}
 
+			IsStartup() ? SetStartupCheckmark(hWnd, true) : SetStartupCheckmark(hWnd, false);
+
 			return 0;
 		}
 	case WM_CLOSE:
@@ -383,6 +525,10 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
             case ID__EXIT:
 				PostQuitMessage(0);
                 return 0;
+            case ID_OPTIONS_RUNATSTARTUP:
+	            {
+            		ToggleStartup(hWnd);
+	            }
             default:
                 return 0;
             }
--- runner/resource.h
+++ runner/resource.h
@@ -4,15 +4,16 @@
 //
 #define IDI_ICON1                       101
 #define IDR_MENU1                       102
+#define ID__EXIT                        1028
+#define ID_OPTIONS_RUNATSTARTUP         40003
 #define ID__SHOW						WM_USER + 3
-#define ID__EXIT                        WM_USER + 4
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        103
-#define _APS_NEXT_COMMAND_VALUE         40003
+#define _APS_NEXT_COMMAND_VALUE         40004
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
