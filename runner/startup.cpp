e3b2e7cb1b80fe548e5399814a84f4b10a9659b7
ZMathias <zagoni.matyas-elemer@bathory.ro>
2023-04-11 17:55:27 +0300

    Minor changes.

--- runner/startup.cpp
+++ runner/startup.cpp
@@ -1,4 +1,11 @@
+// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com
+
 #include "startup.hpp"
+
+#define PREDEF_KEY HKEY_LOCAL_MACHINE
+#define REGISTRY_KEY L"Software\\Microsoft\\Windows\\CurrentVersion\\Run"
+#define REGISTRY_VALUE L"Consoleidator"
 
 BOOL AddToStartup()
 {
@@ -6,32 +13,48 @@ BOOL AddToStartup()
 	GetModuleFileNameW(nullptr, wPathToImage, 500);
 
 	HKEY hKey{};
-	LRESULT lResult = 
-		RegOpenKeyExW(HKEY_CURRENT_USER, L"Software\\Microsoft\\Windows\\CurrentVersion\\Run", NULL, KEY_ALL_ACCESS, &hKey);
+	LRESULT lResult =
+		RegOpenKeyExW(
+			PREDEF_KEY,
+			REGISTRY_KEY,
+			NULL,
+			KEY_ALL_ACCESS,
+			&hKey
+		);
 
-	if (lResult != ERROR_SUCCESS) 
+	if (lResult != ERROR_SUCCESS)
 	{
-	    if (lResult == ERROR_FILE_NOT_FOUND) {
-	        MessageBoxW(nullptr, L"Unable to add to write startup registry.\nCheck if the program has enough privileges.", L"Error", MB_ICONERROR);
+		if (lResult == ERROR_FILE_NOT_FOUND)
+		{
+			MessageBoxW(
+				nullptr, L"Unable to add to write startup registry.\nCheck if the program has enough privileges.",
+				L"Error", MB_ICONERROR);
 			logger::LogError("Registry key not found while opening startup registry key", __FILE__, __LINE__);
-	    	return TRUE;
-	    } 
-	    else {
-	        MessageBoxW(nullptr, L"Unable to add to write startup registry.\nCheck if the program has enough privileges.", L"Error", MB_ICONERROR);
-			logger::LogError("Access denied writing to registry.", __FILE__, __LINE__);
-	        return FALSE;
-	    }
-	}
-	
-	lResult = RegSetValueExW(hKey, L"Consoleidator", NULL, REG_SZ, (LPBYTE)wPathToImage, sizeof wPathToImage);
-
-	if (lResult != ERROR_SUCCESS) 
-	{
-        MessageBoxW(nullptr, L"Unable to add to write startup registry.\nCheck if the program has enough privileges.", L"Error", MB_ICONERROR);
+			return TRUE;
+		}
+		MessageBoxW(nullptr, L"Unable to add to write startup registry.\nCheck if the program has enough privileges.",
+		            L"Error", MB_ICONERROR);
 		logger::LogError("Access denied writing to registry.", __FILE__, __LINE__);
-        return FALSE;
+		return FALSE;
 	}
 
+	lResult = RegSetValueExW(
+		hKey,
+		REGISTRY_VALUE,
+		NULL, REG_SZ,
+		(LPBYTE)wPathToImage,
+		sizeof wPathToImage
+	);
+
+	if (lResult != ERROR_SUCCESS)
+	{
+		MessageBoxW(nullptr, L"Unable to add to write startup registry.\nCheck if the program has enough privileges.",
+		            L"Error", MB_ICONERROR);
+		logger::LogError("Access denied writing to registry.", __FILE__, __LINE__);
+		return FALSE;
+	}
+
+	CloseHandle(hKey);
 	return TRUE;
 }
 
@@ -41,63 +64,60 @@ BOOL RemoveFromStartup()
 	GetModuleFileNameW(nullptr, wPathToImage, 500);
 
 	HKEY hKey{};
-	LRESULT lResult = 
-		RegOpenKeyExW(HKEY_CURRENT_USER, L"Software\\Microsoft\\Windows\\CurrentVersion\\Run", NULL, KEY_ALL_ACCESS, &hKey);
+	LRESULT lResult =
+		RegOpenKeyExW(
+			PREDEF_KEY,
+			REGISTRY_KEY,
+			NULL,
+			KEY_ALL_ACCESS,
+			&hKey
+		);
 
-	if (lResult != ERROR_SUCCESS) 
+	if (lResult != ERROR_SUCCESS)
 	{
-	    if (lResult == ERROR_FILE_NOT_FOUND) {
-	        MessageBoxW(nullptr, L"Unable to delete from startup registry.\nCheck if the program has enough privileges.", L"Error", MB_ICONERROR);
+		if (lResult == ERROR_FILE_NOT_FOUND)
+		{
+			MessageBoxW(
+				nullptr, L"Unable to delete from startup registry.\nCheck if the program has sufficient privileges.",
+				L"Error", MB_ICONERROR);
 			logger::LogError("Registry key not found while opening startup registry key", __FILE__, __LINE__);
-	    	return TRUE;
-	    } 
-	    else {
-	        MessageBoxW(nullptr, L"Unable to delete from startup registry.\nCheck if the program has enough privileges.", L"Error", MB_ICONERROR);
-			logger::LogError("Access denied writing to registry.", __FILE__, __LINE__);
-	        return FALSE;
-	    }
+			CloseHandle(hKey);
+			return TRUE;
+		}
+		MessageBoxW(nullptr, L"Unable to delete from startup registry.\nCheck if the program has sufficient privileges.",
+		            L"Error", MB_ICONERROR);
+		logger::LogError("Access denied writing to registry.", __FILE__, __LINE__);
+		CloseHandle(hKey);
+		return FALSE;
 	}
 
-	lResult = RegDeleteValueW(hKey, L"Consoleidator");
+	lResult = RegDeleteValueW(hKey, REGISTRY_VALUE);
 
-	if (lResult != ERROR_SUCCESS) 
+	if (lResult != ERROR_SUCCESS)
 	{
-		if (lResult == ERROR_FILE_NOT_FOUND) {
-			MessageBoxW(nullptr, L"Unable to delete from startup registry.\nIt probably wasn't present.\nCheck the log for more information.", L"Error", MB_ICONERROR);
-			logger::LogError("Unable to delete value \"Consoleidator\" due to reason: ERROR_FILE_NOT_FOUND", __FILE__, __LINE__);
-			return FALSE;
-		} else {
-			MessageBoxW(nullptr, L"Unable to delete from startup registry.\nCheck if the program has enough privileges.", L"Error", MB_ICONERROR);
-			logger::LogError("Access denied writing to registry.", __FILE__, __LINE__);
+		if (lResult == ERROR_FILE_NOT_FOUND)
+		{
+			MessageBoxW(
+				nullptr,
+				L"Unable to delete from startup registry.\nIt probably wasn't present.\nCheck the log for more information.",
+				L"Error", MB_ICONERROR);
+			logger::LogError("Unable to delete value \"Consoleidator\" due to reason: ERROR_FILE_NOT_FOUND", __FILE__,
+			                 __LINE__);
+			CloseHandle(hKey);
 			return FALSE;
 		}
-        return FALSE;
+		MessageBoxW(nullptr, L"Unable to delete from startup registry.\nCheck if the program has sufficient privileges.",
+		            L"Error", MB_ICONERROR);
+		logger::LogError("Access denied writing to registry.", __FILE__, __LINE__);
+		CloseHandle(hKey);
+		return FALSE;
 	}
 
+	CloseHandle(hKey);
 	return TRUE;
 }
 
-void ToggleStartup(HWND hWnd)
-{
-	HMENU hmenu = GetMenu(hWnd);
-
-	MENUITEMINFO menuItem{};
-	menuItem.cbSize = sizeof MENUITEMINFO;
-	menuItem.fMask = MIIM_STATE;
-
-	GetMenuItemInfo(hmenu, ID_OPTIONS_RUNATSTARTUP, FALSE, &menuItem);
-
-	if (menuItem.fState == MFS_CHECKED) {
-		// Checked, uncheck it
-		menuItem.fState = RemoveFromStartup() ? MFS_UNCHECKED : MFS_CHECKED;
-	} else {
-		// Unchecked, check it
-		menuItem.fState = AddToStartup() ? MFS_CHECKED : MFS_UNCHECKED;
-	}
-
-	SetMenuItemInfo(hmenu, ID_OPTIONS_RUNATSTARTUP, FALSE, &menuItem);
-}
-
+// disables the "Run at startup" menu item
 void SetStartupCheckboxGreyed(HWND hWnd, bool state)
 {
 	// toggle menu item
@@ -112,59 +132,106 @@ void SetStartupCheckboxGreyed(HWND hWnd, bool state)
 	SetMenuItemInfo(hMenu, 0, FALSE, &menuInfo);
 }
 
-void SetStartupCheckmark(HWND hWnd, bool state)
+// toggles the checkmark state depending on the current state
+void ToggleStartup(HWND hWnd)
 {
-	HMENU hmenu = GetMenu(hWnd);
+	HMENU hMenu = GetMenu(hWnd);
 
 	MENUITEMINFO menuItem{};
 	menuItem.cbSize = sizeof MENUITEMINFO;
 	menuItem.fMask = MIIM_STATE;
 
-	GetMenuItemInfo(hmenu, ID_OPTIONS_RUNATSTARTUP, FALSE, &menuItem);
+	GetMenuItemInfo(hMenu, ID_OPTIONS_RUNATSTARTUP, FALSE, &menuItem);
+
+	if (menuItem.fState == MFS_CHECKED)
+	{
+		// Checked, uncheck it
+		menuItem.fState = RemoveFromStartup() ? MFS_UNCHECKED : MFS_CHECKED;
+	}
+	else
+	{
+		// Unchecked, check it
+		menuItem.fState = AddToStartup() ? MFS_CHECKED : MFS_UNCHECKED;
+	}
+
+	SetMenuItemInfo(hMenu, ID_OPTIONS_RUNATSTARTUP, FALSE, &menuItem);
+}
+
+// sets the checkmark in the menu according to the state parameter
+void SetStartupCheckmark(HWND hWnd, bool state)
+{
+	HMENU hMenu = GetMenu(hWnd);
+
+	MENUITEMINFO menuItem{};
+	menuItem.cbSize = sizeof MENUITEMINFO;
+	menuItem.fMask = MIIM_STATE;
+
+	GetMenuItemInfo(hMenu, ID_OPTIONS_RUNATSTARTUP, FALSE, &menuItem);
 
 	menuItem.fState = state ? MFS_CHECKED : MFS_UNCHECKED;
 
-	SetMenuItemInfo(hmenu, ID_OPTIONS_RUNATSTARTUP, FALSE, &menuItem);
+	SetMenuItemInfo(hMenu, ID_OPTIONS_RUNATSTARTUP, FALSE, &menuItem);
 }
 
-BOOL IsStartup()
+// queries if the image is set to start with the system
+int IsStartup()
 {
 	HKEY hKey{};
-	LRESULT lResult = 
-		RegOpenKeyExW(HKEY_CURRENT_USER, L"Software\\Microsoft\\Windows\\CurrentVersion\\Run", NULL, KEY_ALL_ACCESS, &hKey);
+	LRESULT lResult =
+		RegOpenKeyExW(
+			PREDEF_KEY,
+			REGISTRY_KEY,
+			NULL,
+			KEY_ALL_ACCESS,
+			&hKey
+		);
 
-	if (lResult != ERROR_SUCCESS) 
+	if (lResult != ERROR_SUCCESS)
 	{
-	    if (lResult == ERROR_FILE_NOT_FOUND) {
-			MessageBoxW(nullptr, L"Unable to check for startup presence in the registry. The startup button has been disabled.", L"Error", MB_ICONWARNING);
-			logger::LogError("Registry key not found while opening startup registry key", __FILE__, __LINE__);
-	    	return TRUE;
-	    } 
-	    else {
-	    	MessageBoxW(nullptr, L"Unable to check for startup presence in the registry. The startup button has been disabled.", L"Error", MB_ICONWARNING);
-			// disable the startup checkbox
-			logger::LogError("Access denied writing to registry.", __FILE__, __LINE__);
-	        return FALSE;
-	    }
+		// couldn't open registry path
+		if (lResult == ERROR_FILE_NOT_FOUND)
+		{
+			logger::LogError("Registry key not found while opening startup registry key,", __FILE__, __LINE__);
+			CloseHandle(hKey);
+			return -1;
+		}
+
+		// disable the startup checkbox
+		logger::LogError(
+			"Access denied writing to registry. GetLastError returned: " + std::to_string(GetLastError()),
+			__FILE__,
+			__LINE__
+		);
+		CloseHandle(hKey);
+		// TODO: fix user alerts
+		// the caller should dispatch alerts based on return value
+		return -1;
 	}
 
 	lResult = RegQueryValueExW(
 		hKey,
-		L"Consoleidator",
+		REGISTRY_VALUE,
 		nullptr,
 		nullptr,
 		nullptr,
 		nullptr
 	);
 
+	// this is business logic, not error handling
 	if (lResult != ERROR_SUCCESS)
 	{
-		if (lResult == ERROR_FILE_NOT_FOUND) {
-			return FALSE;
-		} else {
-			logger::LogError("An error occured while checking for startup presence in the registry.", __FILE__, __LINE__);
+		if (lResult == ERROR_FILE_NOT_FOUND)
+		{
+			// the value is not present, thus the program is not set to start with the system
+			CloseHandle(hKey);
 			return FALSE;
 		}
+		logger::LogError("An error occured while checking for startup presence in the registry.", __FILE__, __LINE__);
+		CloseHandle(hKey);
+		return FALSE;
 	}
-	 return TRUE;
+
+	// the value is present, thus the program is set to start with the system
+	CloseHandle(hKey);
+	return TRUE;
 }
